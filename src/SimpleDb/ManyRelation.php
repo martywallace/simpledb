935028131b9de6c4fb053724c37c621b26ec1949
Marty Wallace <martyaced@gmail.com>
2017-01-11 17:56:24 +1100

    More relations.

---
+++ src/SimpleDb/ManyRelation.php
@@ -0,0 +1,22 @@
+<?php namespace SimpleDb;
+
+class ManyRelation extends Relation {
+
+	private $_localColumn;
+	private $_externalTable;
+	private $_externalColumn;
+	private $_model;
+
+	public function __construct($localColumn, $externalTable, $externalColumn, $model = null) {
+		$this->_localColumn = $localColumn;
+		$this->_externalTable = $externalTable;
+		$this->_externalColumn = $externalColumn;
+		$this->_model = $model;
+	}
+
+	public function fetch(Model $model) {
+		$rows = Database::get()->all('SELECT * FROM ' . $this->_externalTable . ' WHERE ' . $this->_externalColumn . ' = ?', [$model->get($this->_localColumn)]);
+		return !empty($this->_model) ? $rows->populate($this->_model) : $rows;
+	}
+
+}
